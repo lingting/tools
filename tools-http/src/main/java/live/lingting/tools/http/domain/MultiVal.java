b1ae7c147013bd6973dca076b64f4016b4186366
lingting <sunlisten.gzm@gmail.com>
2021-12-27 13:50:12 +0800

    :bug: 修复 MultiVal 在urlBuild时. 无法识别

--- tools-http/src/main/java/live/lingting/tools/http/domain/MultiVal.java
+++ tools-http/src/main/java/live/lingting/tools/http/domain/MultiVal.java
@@ -1,12 +1,14 @@
 package live.lingting.tools.http.domain;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 
 /**
  * @author lingting
  */
-public class MultiVal {
+public class MultiVal implements Iterable<Object> {
 
 	private final List<Object> list = new ArrayList<>();
 
@@ -14,8 +16,17 @@ public class MultiVal {
 		list.add(obj);
 	}
 
+	public void addAll(Collection<Object> objects) {
+		list.addAll(objects);
+	}
+
 	public List<Object> values() {
 		return list;
+	}
+
+	@Override
+	public Iterator<Object> iterator() {
+		return list.iterator();
 	}
 
 }
